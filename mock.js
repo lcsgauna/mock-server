7728abf4321756c0240110e2ce26715578bac015
Lucas Gauna Samudio <lucasgsamudio@gmail.com>
2024-03-18 21:51:31 -0400

    feat: Adicionando mock data generator

---
+++ mock.js
@@ -0,0 +1,36 @@
+const faker  = require('@faker-js/faker');
+const mocker = require('mocker-data-generator').default
+const fs = require('node:fs');
+const path = require('node:path');
+
+const filePath = path.join(__dirname,'input','db.json')
+
+let user = {
+    id:{
+        incrementalId: 0
+    },
+    name: {
+        faker: 'faker.person.fullName()'
+    }
+}
+
+mocker()
+    .addGenerator('faker', faker)
+    .schema('users', user,10)
+    .build(function(error,data) {
+
+        if(error){
+            throw error
+        }
+
+
+        fs.writeFile(filePath, JSON.stringify(data,null,2), 'utf8', (err) => {
+            if (err) {
+              console.log('Erro ao criar o arquivo: ',err);
+              return;
+            }
+            console.log('Criado o arquivo db.json com sucesso');
+          });
+    });
+
+    
